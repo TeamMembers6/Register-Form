9da1ab60d0fa21677f54cb27d86f777575077c9a
Yaswanth <22jn1a05f9@svcn.ac.in>
2025-06-21 11:38:34 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
 
-import App from './App.jsx'
+// import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<App></App>
+<>
+  <h1>These is heading.</h1>
+</>
+  
 )
